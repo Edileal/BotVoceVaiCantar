7e661b450c502672be610645885b3b7929dc02f4
Edivandro <edileal1@gmail.com>
2023-07-07 16:57:38 -0300

    Interface

--- BotVoceVaiCantar.Domain/Contracts/CantorRequest.cs
+++ BotVoceVaiCantar.Domain/Contracts/CantorRequest.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BotVoceVaiCantar.Domain.Contracts
+﻿namespace BotVoceVaiCantar.Domain.Contracts
 {
     public class CantorRequest
     {
         public string Name { get; set; }
-        public int Telefone { get; set; }
+        public string Telefone { get; set; }
         public DateTime Data { get; set; }
     }
 }
