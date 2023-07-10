004b767906d8baa848b3bd2a42375beeb62dfc7b
Edivandro <edileal1@gmail.com>
2023-07-10 18:31:52 -0300

    Pomelo configurado

--- BotVoceVaiCantar.Domain/Entities/Cantor.cs
+++ BotVoceVaiCantar.Domain/Entities/Cantor.cs
@@ -2,7 +2,7 @@
 {
     public class Cantor
     {
-        public int Id { get; set; }
+        public Guid Id { get; init; } = Guid.NewGuid();
         public string Name { get; set; }
         public string Telefone { get; set; }
         public DateTime Data { get; set; }
