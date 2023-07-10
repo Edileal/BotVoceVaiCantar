ecdaa3fd29605cdd62fe69f13928b344685140f2
Edivandro <edileal1@gmail.com>
2023-07-10 17:42:42 -0300

    CantorController feita

--- BotVoceVaiCantar/Controllers/CantorController.cs
+++ BotVoceVaiCantar/Controllers/CantorController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BotVoceVaiCantar.Domain.Contracts;
+using BotVoceVaiCantar.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BotVoceVaiCantar.Controllers
 {
@@ -6,5 +8,115 @@ namespace BotVoceVaiCantar.Controllers
     [Route("[controller]")]
     public class CantorController : ControllerBase
     {
+        private readonly ICantorService _cantorService;
+
+        public CantorController(ICantorService cantorService)
+        {
+            _cantorService = cantorService;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<CantorResponse>> GetById(Guid id)
+        {
+            try
+            {
+                var result = await _cantorService.ObterPorIdAsync(id);
+                return Ok(result);
+            }
+            catch (ArgumentException exception)
+            {
+                return NotFound(exception.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<CantorResponse>>> Get()
+        {
+            try
+            {
+                var result = await _cantorService.ObterTodosAsync();
+                return Ok(result);
+            }
+            catch (ArgumentException exception)
+            {
+                return NotFound(exception.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<CantorResponse>> Post([FromBody] CantorRequest usuario)
+        {
+            try
+            {
+                var result = await _cantorService.CriarAsync(usuario);
+                return Created(nameof(Post), result);
+            }
+            catch (ArgumentException exception)
+            {
+                return BadRequest(exception.Message);
+            }
+            catch (Exception ex) { return StatusCode(StatusCodes.Status500InternalServerError, ex.InnerException); }
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public async Task<ActionResult<CantorResponse>> Put([FromBody] CantorRequest request, [FromRoute] Guid id)
+        {
+            try
+            {
+                var result = await _cantorService.AtualizarAsync(id, request);
+                return Ok(result);
+            }
+            catch (ArgumentException exception)
+            {
+                return NotFound(exception.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> Delete([FromRoute] Guid id)
+        {
+            try
+            {
+                await _cantorService.DeletarAsync(id);
+                return NoContent();
+            }
+            catch (ArgumentException exception)
+            {
+                return NotFound(exception.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
     }
 }
