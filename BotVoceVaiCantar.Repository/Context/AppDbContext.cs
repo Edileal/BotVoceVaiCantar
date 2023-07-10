361b38071994cd32969c6409337c5017663382e6
Edivandro <edileal1@gmail.com>
2023-07-10 12:21:10 -0300

    CantorRepository ok

---
+++ BotVoceVaiCantar.Repository/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BotVoceVaiCantar.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BotVoceVaiCantar.Repository.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected AppDbContext(DbContextOptions<AppDbContext> options) : base (options) { }
+
+        public DbSet<Cantor> Cantor { get; set; }
+        
+    }
+}
