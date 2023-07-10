f4ae16008a9f2f11b26a9996dd68fcf0c7427abc
Edivandro <edileal1@gmail.com>
2023-07-10 16:16:58 -0300

    Mappers construídos

---
+++ BotVoceVaiCantar/Profiles/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BotVoceVaiCantar.Domain.Contracts;
+using BotVoceVaiCantar.Domain.Entities;
+
+namespace BotVoceVaiCantar.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+            CreateMap<CantorRequest, Cantor>().ReverseMap();
+            CreateMap<CantorResponse, Cantor>().ReverseMap();
+        }
+    }
+}
--- BotVoceVaiCantar/Program.cs
+++ BotVoceVaiCantar/Program.cs
@@ -1,3 +1,7 @@
+using BotVoceVaiCantar.Domain.Interfaces;
+using BotVoceVaiCantar.Repository.Repositories;
+using BotVoceVaiCantar.Service.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +11,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#region build service e repository
+builder.Services.AddScoped<ICantorService, CantorService>();
+builder.Services.AddScoped<ICantorRepository, CantorRepository>();
+#endregion
+
+#region mapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+#endregion
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
