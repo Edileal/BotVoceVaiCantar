913048965cb78e8349f9a404530bc298ba12cce5
Edivandro <edileal1@gmail.com>
2023-07-12 16:32:03 -0300

    Contracts

---
+++ BotVoceVaiCantar.Domain/Contracts/AutenticacaoRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BotVoceVaiCantar.Domain.Contracts
+{
+    public class AutenticacaoRequest
+    {
+        [Required(ErrorMessage = "O campo 'Nome' é obrigatorio")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo 'Senha' é obrigatorio")]
+        public string Senha { get; set; }
+    }
+}
---
+++ BotVoceVaiCantar.Domain/Interfaces/IAutenticacaoService.cs
@@ -0,0 +1,9 @@
+﻿using BotVoceVaiCantar.Domain.Contracts;
+
+namespace BotVoceVaiCantar.Domain.Interfaces
+{
+    public interface IAutenticacaoService
+    {
+        Task<AutenticacaoResponse> AutenticarAsync(string nome, string senha);
+    }
+}
